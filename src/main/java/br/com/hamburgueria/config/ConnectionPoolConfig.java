53a2378c585df18bf41604ec2d4ccf7c3266aa78
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-03 22:24:48 -0300

    Adicionar configuração da pool de conexões com o banco de dados H2

--- src/main/java/br/com/hamburgueria/config/ConnectionPoolConfig.java
+++ src/main/java/br/com/hamburgueria/config/ConnectionPoolConfig.java
@@ -1,2 +1,36 @@
-package br.com.hamburgueria.config;public class ConnectionPoolConfig {
+package br.com.hamburgueria.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionPoolConfig {
+
+    private static BasicDataSource dataSource;
+
+    private ConnectionPoolConfig() {
+        getDataSource();
+    }
+
+    private static BasicDataSource getDataSource() {
+
+        if (dataSource == null) {
+            dataSource = new BasicDataSource();
+            dataSource.setUrl("jdbc:h2:~/test");
+            dataSource.setUsername("sa");
+            dataSource.setPassword("sa");
+            dataSource.setMinIdle(5);
+            dataSource.setMaxIdle(10);
+            dataSource.setMaxTotal(50);
+
+            System.out.println("New connection pool created succesfully");
+        }
+
+        return dataSource;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
 }
