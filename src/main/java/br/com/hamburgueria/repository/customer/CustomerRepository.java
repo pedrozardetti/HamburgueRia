bdb033d0c48c41c7338159c8c7eda20de984ce5d
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-03 22:30:39 -0300

    Adicionar modelo Customer com atributos e métodos

--- src/main/java/br/com/hamburgueria/repository/customer/CustomerRepository.java
+++ src/main/java/br/com/hamburgueria/repository/customer/CustomerRepository.java
@@ -1,2 +1,30 @@
-package br.com.hamburgueria.dao.customer;public class CustomerDao {
+package br.com.hamburgueria.repository.customer;
+
+import br.com.hamburgueria.config.ConnectionPoolConfig;
+import br.com.hamburgueria.model.Customer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class CustomerRepository {
+
+    public void createCustomer(Customer customer) {
+        String SQL = "INSERT INTO CUSTOMER (NAME, EMAIL, PASSWORD, TYPE) VALUES (?, ?, ?, ?)";
+
+        try {
+            Connection con = ConnectionPoolConfig.getConnection();
+            PreparedStatement preparedStatement = con.prepareStatement(SQL);
+            preparedStatement.setString(1, customer.getName());
+            preparedStatement.setString(2, customer.getEmail());
+            preparedStatement.setString(3, customer.getPassword());
+            preparedStatement.setString(4, customer.getType().toString());
+            preparedStatement.execute();
+
+            con.close();
+
+        } catch (Exception e) {
+            System.out.println("A criação do customer não deu certo!");
+        }
+
+    }
 }
