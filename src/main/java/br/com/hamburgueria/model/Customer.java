f99478b2a2ee807ad4584e787b95db8dd81c7390
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-03 22:27:12 -0300

    Adicionar repositório de clientes para criação de novos registros e melhorar o fechamento de recursos

--- src/main/java/br/com/hamburgueria/model/Customer.java
+++ src/main/java/br/com/hamburgueria/model/Customer.java
@@ -1,2 +1,64 @@
-package br.com.hamburgueria.model;public class Customer {
+package br.com.hamburgueria.model;
+
+import br.com.hamburgueria.model.enums.TypeCustomer;
+
+import java.util.UUID;
+
+public class Customer {
+
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+    private TypeCustomer type;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String email, String password, TypeCustomer type) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public TypeCustomer getType() {
+        return type;
+    }
+
+    public void setType(TypeCustomer type) {
+        this.type = type;
+    }
 }
