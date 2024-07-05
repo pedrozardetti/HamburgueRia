377a072f7af95b93f7a4321962bbce3e94cc4223
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-05 20:57:44 -0300

    feat: classe de criptografia que converte a senha para MD5

--- src/main/java/br/com/hamburgueria/security/Criptografia.java
+++ src/main/java/br/com/hamburgueria/security/Criptografia.java
@@ -1,2 +1,19 @@
-package br.com.hamburgueria.security;public class Criptografia {
+package br.com.hamburgueria.security;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Criptografia {
+
+    public static String converterParaMD5(String senha) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(senha.getBytes());
+        byte[] digest = md.digest();
+        String resultado = new String(Hex.encodeHex(digest));
+        return resultado;
+    }
+
+
 }
