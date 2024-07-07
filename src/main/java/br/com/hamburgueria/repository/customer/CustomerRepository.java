285b4b6f043e13e37aacb21fc5553983d0f7cd4c
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-07 19:44:56 -0300

    feat: classe findCustomerByEmail para verificar se o cliente existe no BD para obter a senha criptografada

--- src/main/java/br/com/hamburgueria/repository/customer/CustomerRepository.java
+++ src/main/java/br/com/hamburgueria/repository/customer/CustomerRepository.java
@@ -2,9 +2,11 @@ package br.com.hamburgueria.repository.customer;
 
 import br.com.hamburgueria.config.ConnectionPoolConfig;
 import br.com.hamburgueria.model.Customer;
+import br.com.hamburgueria.model.enums.TypeCustomer;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class CustomerRepository {
 
@@ -26,5 +28,33 @@ public class CustomerRepository {
             System.out.println("A criação do customer não deu certo!");
         }
 
+    }
+
+
+    // Esse método verifica se o cliente existe com base no e-mail para obter a senha criptografada que está no BD
+    public Customer findCustomerByEmail(String email) {
+        String SQL = "SELECT * FROM CUSTOMER WHERE EMAIL = ?";
+        Customer customer = null;
+
+        try {
+            Connection con = ConnectionPoolConfig.getConnection();
+            PreparedStatement preparedStatement = con.prepareStatement(SQL);
+            preparedStatement.setString(1, email);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            if (rs.next()) {
+                String name = rs.getString("NAME");
+                String password = rs.getString("PASSWORD");
+                TypeCustomer type = TypeCustomer.valueOf(rs.getString("TYPE"));
+                customer = new Customer(name, email, password, type);
+            }
+
+            con.close();
+
+        } catch (Exception e) {
+            System.out.println("A busca do cliente não deu certo!");
+        }
+
+        return customer;
     }
 }
