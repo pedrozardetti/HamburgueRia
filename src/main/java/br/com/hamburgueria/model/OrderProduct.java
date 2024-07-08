a0c6bad7e96ab5131b71b8088ebe7f3415be6eee
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-08 18:42:36 -0300

    classe OrderProduct com seus devidos relacionamentos, getters e setters

---
+++ src/main/java/br/com/hamburgueria/model/OrderProduct.java
@@ -0,0 +1,53 @@
+package br.com.hamburgueria.model;
+
+import java.util.UUID;
+
+public class OrderProduct {
+
+    private UUID id;
+    private UUID orderId;
+    private UUID productId;
+    private int quantity;
+
+    public OrderProduct() {
+    }
+
+    public OrderProduct(UUID id, UUID orderId, UUID productId, int quantity) {
+        this.id = id;
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(UUID orderId) {
+        this.orderId = orderId;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public void setProductId(UUID productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
