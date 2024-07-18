abe6b55a418ca8c6844fa33fe3a62de5d3f53592
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-18 17:11:41 -0300

    feat: método doGet implementado

--- src/main/java/br/com/hamburgueria/servlet/HomeServlet.java
+++ src/main/java/br/com/hamburgueria/servlet/HomeServlet.java
@@ -1,2 +1,18 @@
-package br.com.hamburgueria.servlet;public class HomeServlet {
+package br.com.hamburgueria.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("home.jsp").forward(req, resp);
+    }
+
 }
