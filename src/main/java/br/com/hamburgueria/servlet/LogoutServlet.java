df1cae2c3b345ee914ce43048900ea82f76998c6
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-08-06 19:37:07 -0300

    feat: classe com método doGet para realizar logout

--- src/main/java/br/com/hamburgueria/servlet/LogoutServlet.java
+++ src/main/java/br/com/hamburgueria/servlet/LogoutServlet.java
@@ -1,2 +1,26 @@
-package br.com.hamburgueria.servlet;public class LogoutServlet {
+
+package br.com.hamburgueria.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //Ao fazer o doGet, invalidamos a sessão do usuário que está fazendo a requisição. Ele será deslogado
+        req.getSession().invalidate();
+
+        // É usado para armazenar temporariamente uma mensagem de sucesso na requisição HTTP atual
+        req.setAttribute("message", "Success on logout");
+
+
+        req.getRequestDispatcher("login.jsp").forward(req, resp);
+    }
 }
