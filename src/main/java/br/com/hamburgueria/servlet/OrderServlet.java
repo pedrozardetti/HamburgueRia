c19c3dfecd9311deaaf891e9412986d46c3b816b
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-08-06 19:39:18 -0300

    feat: adicionar lógica para criação de pedidos e itens do carrinho na OrderServlet

--- src/main/java/br/com/hamburgueria/servlet/OrderServlet.java
+++ src/main/java/br/com/hamburgueria/servlet/OrderServlet.java
@@ -1,2 +1,49 @@
-package br.com.hamburgueria.servlet;public class OrderServlet {
+package br.com.hamburgueria.servlet;
+
+import br.com.hamburgueria.model.Customer;
+import br.com.hamburgueria.model.Order;
+import br.com.hamburgueria.model.OrderProduct;
+import br.com.hamburgueria.model.Product;
+import br.com.hamburgueria.repository.order.OrderRepository;
+import br.com.hamburgueria.repository.orderproduct.OrderProductRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@WebServlet("/customer-filter/order")
+public class OrderServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Lógica da servlet
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession httpSession = req.getSession();
+        List<OrderProduct> cart = (List<OrderProduct>) httpSession.getAttribute("cart");
+
+        UUID orderId = UUID.randomUUID();
+        double maxPrice = 0;
+        for (OrderProduct item : cart) {
+            item.setOrderId(orderId);
+            new OrderProductRepository().create(item);
+            maxPrice += item.getPrice() * item.getQuantity();
+        }
+        Customer user = (Customer) httpSession.getAttribute("user");
+        Order order = new Order(orderId, maxPrice, new Date(), user.getId());
+
+        new OrderRepository().create(order);
+        httpSession.removeAttribute("cart");
+        resp.sendRedirect("/home");
+    }
 }
+
