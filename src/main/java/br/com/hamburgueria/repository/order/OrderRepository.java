78d962ce075a108b2ec44bdce8edce6a51460439
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-08-06 19:25:33 -0300

    feat: classe order para criação de pedidos

--- src/main/java/br/com/hamburgueria/repository/order/OrderRepository.java
+++ src/main/java/br/com/hamburgueria/repository/order/OrderRepository.java
@@ -1,2 +1,34 @@
-package br.com.hamburgueria.repository.order;public class OrderRepository {
+package br.com.hamburgueria.repository.order;
+
+import br.com.hamburgueria.config.ConnectionPoolConfig;
+import br.com.hamburgueria.model.Order;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+
+public class OrderRepository {
+
+    public void create(Order order) {
+        String SQL = "INSERT INTO ORDER (ID, MAX_PRICE, CREATED_AT, CUSTOMER_ID) VALUES (?, ?, ?, ?)";
+
+        try {
+            Connection connection = ConnectionPoolConfig.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setObject(1, order.getId());
+            preparedStatement.setDouble(2, order.getMaxPrice());
+            preparedStatement.setDate(3, (Date) order.getCreatedAt());
+            preparedStatement.setObject(4, order.getCustomerId());
+            preparedStatement.execute();
+            System.out.println("Deu certo!");
+
+            connection.close();
+
+            return;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+    }
+
 }
