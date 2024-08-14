4c1512fab2748d0aef2073cfd3f74fd608d3d528
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-08-14 16:19:26 -0300

    fix

--- src/main/java/br/com/hamburgueria/servlet/OrderServlet.java
+++ src/main/java/br/com/hamburgueria/servlet/OrderServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,18 +30,25 @@ public class OrderServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession httpSession = req.getSession();
         List<OrderProduct> cart = (List<OrderProduct>) httpSession.getAttribute("cart");
+        UUID addressId = UUID.fromString(req.getParameter("selectedAddress").toString());
+
 
         UUID orderId = UUID.randomUUID();
         double maxPrice = 0;
         for (OrderProduct item : cart) {
-            item.setOrderId(orderId);
-            new OrderProductRepository().create(item);
             maxPrice += item.getPrice() * item.getQuantity();
         }
         Customer user = (Customer) httpSession.getAttribute("user");
-        Order order = new Order(orderId, maxPrice, new Date(), user.getId());
+        Order order = new Order(orderId, maxPrice, new Date(System.currentTimeMillis()), user.getId(), addressId);
 
         new OrderRepository().create(order);
+
+        for (OrderProduct item : cart) {
+            item.setOrderId(orderId);
+            new OrderProductRepository().create(item);
+
+        }
+
         httpSession.removeAttribute("cart");
         resp.sendRedirect("/home");
     }
